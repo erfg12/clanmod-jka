a14b3e1989fd664361bfbc476a9ff5f0f70f5208
Jacob Fliss <jake@newagesoldier.com>
2016-03-28 18:36:42 -0400

    fixed linux sh host files

--- Installer/files/clanmod_server.i386.sh
+++ Installer/files/clanmod_server.i386.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-./openjkded.i386 +set dedicated 2 +set fs_game clanmod +exec server.cfg +exec bots.cfg
+
+./openjkded.i386 +set dedicated 2 +set fs_game japlus +exec server.cfg +exec bots.cfg
