8dcaaf6489ad456cc9c8375594e8326de49f1010
erfg12 <newgmobile@gmail.com>
2018-03-14 18:24:32 -0400

    updated to vs 2017, added gitignore file

--- cleanup.bat
+++
@@ -1,8 +0,0 @@
-rmdir /s /q "codemp\Release"
-rmdir /s /q "codemp\Debug"
-rmdir /s /q "codemp\ipch\debug"
-rmdir /s /q "codemp\ipch\release"
-rmdir /s /q "codemp\.vs"
-del "codemp\JKA_mp(SDK).sdf"
-del "codemp\JKA_mp(SDK).VC.db"
-del /S /F /AH *.suo
