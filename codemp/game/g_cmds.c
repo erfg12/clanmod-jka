8699e7834d4fb7934939f94f73d1c487f1cc5a6c
ineedblood <fdiazmalaga@outlook.com>
2018-11-26 22:50:03 +0000

    fix comparinson on hidemotd which blocked cmds

--- codemp/game/g_cmds.c
+++ codemp/game/g_cmds.c
@@ -2774,7 +2774,7 @@ void G_Say( gentity_t *ent, gentity_t *target, int mode, const char *chatText ) 
 	else if (strstr(text, "!stats")) {
 		cmStats(ent, "");
 	}
-	else if (Q_stricmp(text, "!hidemotd")) {
+	else if (strstr(text, "!hidemotd")) {
 		hideMOTD(ent);
 	}
 
